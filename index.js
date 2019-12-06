a07d9efc92c5e53e61ca214e8731309980b27def
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-06 14:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ document.addEventListener("DOMContentLoaded", function() {
   let text=document.getElementById("text");
   text.innerHTML="The DOM has loaded";
 });
- 
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
